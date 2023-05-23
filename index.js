09bd57825cad009eabd8bcbac3e58e3e2b277cd6
zaieon <zoejohn007@gmail.com>
2023-05-24 00:45:27 +0100

    Changed all files and content

---
+++ index.js
@@ -0,0 +1,43 @@
+let todo = (function() {
+
+    let todo = function (id, activity) {
+        
+    }
+
+
+
+})()
+
+
+
+let UIController = (function () {
+
+})()
+    
+    
+
+let controller = (function () {
+
+    let itemBtn = function () {
+        document.querySelector('.delete').addEventListener('click', addTodo)
+        document.querySelector('html').addEventListener('click', function (e) {
+            if (e.keycode === 13) {
+                addTodo()
+            }
+        })
+    }
+
+    let addTodo = function () {
+        
+    }
+
+
+
+
+    return {
+        itemBtn
+
+    }
+    
+})()
+controller.itemBtn()
