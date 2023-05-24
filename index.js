4c442b4d9851795c322552cff4ee36a9038e05cc
zaieon <zoejohn007@gmail.com>
2023-05-24 14:41:18 +0100

    add item to list, clear fields, create item object list

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 let todoController = (function() {
 
     let todo = function (id, activity) {
-        
+        this.id = id
+        this.activity = activity
+    }
+
+    let data = {
+        todos: []
     }
 
     
     return {
-
+        addItem: function (activity) {
+            let newTodo, ID;
+            if (data.todos.length === 0) {
+                ID = 0
+            } else {
+                ID = data.todos.indexOf(data.todos[data.todos.length - 1]) + 1
+            }
+            newTodo = new todo(ID, activity)
+            data.todos.push(newTodo)
+            console.log(data.todos)
+            return newTodo
+            
+        }
     }
 
 
@@ -19,7 +36,8 @@ let UIController = (function () {
 
     let domStrings = {
         todo: '#todo',
-        deleteBtn: '.delete'
+        deleteBtn: '.delete',
+        todoListContainer: '.todo-list-container'
     }
 
     return {
@@ -28,9 +46,23 @@ let UIController = (function () {
             return {
                 activity: document.querySelector(domStrings.todo).value,
             }
-        }
+        },
+        clearField: function () {
+            document.querySelector(domStrings.todo).value = ''
+        },
+        addListItem: function (todoObj) {
+            let action
+            action = `<div class="list-item"><div class="part-one"><button class="delete">Del</button><p class="item-text">${todoObj.activity}</p></div><div class="check"><input type="checkbox" name="checkbox" id="checkbox"></div></div>`
+            
+            setTimeout(() => {
+                document.querySelector(domStrings.todoListContainer).insertAdjacentHTML('beforeend', action)
+            }, 150);
+            
+          }
+
 
     }
+
 })()
     
     
@@ -41,7 +73,6 @@ let controller = (function (tCtrl, UIctrl) {
         document.querySelector('.add-btn').addEventListener('click', addTodo)
         document.querySelector('body').addEventListener('keypress', function (e) {
             if (e.keycode === 13 || e.which === 13) {
-                console.log('btn fired!!')
                 addTodo()
             }
         })
@@ -49,10 +80,19 @@ let controller = (function (tCtrl, UIctrl) {
 
     let addTodo = function () {
         // Get todo values
-        console.log(UIctrl.getTodo())
+        let todoValue
+        todoValue = UIctrl.getTodo()
+        // Clear Fields
+        
+        if (todoValue.activity != '') {
+            console.log(UIctrl.getTodo())
         //store todo values as objects
-
+        tCtrl.addItem(todoValue.activity)
         // add todo values to list
+            UIctrl.addListItem(todoValue)
+            UIctrl.clearField()
+        }
+        
     }
 
 
